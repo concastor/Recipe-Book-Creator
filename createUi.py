6f8c9c51f3095ef174b8d01f8ce0a24674c8f0c3
jake <jake.ejrichardson@gmail.com>
2020-02-07 10:55:00 -0500

    update

--- createUi.py
+++ createUi.py
@@ -1,5 +1,5 @@
-from PyQt5.QtWidgets import QApplication, QHBoxLayout, QVBoxLayout, QLineEdit, QWidget, QLabel
-from PyQt5 import uic, QtWidgets
+from PyQt5.QtWidgets import *
+from PyQt5 import uic, QtWidgets, QtCore
 import sys
 
 
@@ -13,23 +13,32 @@ class createUi(QtWidgets.QMainWindow):
         self.create_base()
 
     def create_base(self):
-        Hlayout = QHBoxLayout()
+        Hlayout = QGridLayout()
         AddVLayout = QVBoxLayout()
         displayV = QVBoxLayout()
 
+        #create the input for the ingredients
         ingInput = QLineEdit(self)
-        ingInput.setPlaceholderText("please enter an ingredient")
+        ingInput.setPlaceholderText("Please enter an ingredient")
+        ingInput.setMaximumWidth(self.window.width()/2)
+        ingInput.setMinimumWidth(self.window.width()/3)
+
         btn = QLabel(self)
+        addBtn = QPushButton(self)
+        addBtn.setText("add")
         btn.setText("holy shit")
 
-        Hlayout.addWidget(ingInput)
-        Hlayout.addWidget(btn)
+        # Hlayout.addWidget(ingInput)
+        # Hlayout.addWidget(btn)
+        # print(self.window.width())
 
-        # Hlayout.addLayout(displayV)
-        # Hlayout.addLayout(AddVLayout)
+        Hlayout.setRowMinimumHeight(0, 20)
+        Hlayout.addWidget(ingInput, 1, 0, QtCore.Qt.AlignCenter)
+        Hlayout.addWidget(btn, 1, 1,  QtCore.Qt.AlignCenter)
+        Hlayout.addWidget(addBtn, 2, 0)
 
         #set the horizontal layout as main
-        widget = QWidget()
+        widget = QWidget(self)
         widget.setLayout(Hlayout)
         self.window.setCentralWidget(widget)
 
